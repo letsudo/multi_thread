a6a2558a804bb8251ba32dc02a5437d4442b57db
姚想 <yx4@meitu.com>
2019-09-21 17:33:13 +0800

    [feature] 由类创建线程

--- lesson-1/main.cpp
+++ lesson-1/main.cpp
@@ -1,19 +1,30 @@
 #include <iostream>
 #include <thread>
 
-void function(){
-    std::cout<<"hi in function"<<std::endl;
-}
+class TA
+{
+public:
+//    int m_i;
+//    TA(int i):m_i(i){
+//        std::cout<<"构造函数"<<std::endl;
+//    }
+    void operator()()
+    {
+        std::cout<<"operator"<<std::endl;
+    }
+};
 
 
 int main(int argc, const char * argv[]) {
-    std::thread t1(function);//创建t1子线程，并执行，functionx是线程执行的入口
     
+    TA ta;
+    std::thread t1(ta);
     //join加入汇合、阻塞
 //    t1.join();//主线程等待t1子线程执行完毕
-    t1.detach();//不等待， 一旦被detach 就不能被join了
+//    t1.detach();//不等待， 一旦被detach 就不能被join了
     if(t1.joinable()){
         t1.join();
     }
+    std::cout<<"af operator in main"<<std::endl;
     return 0;
 }
