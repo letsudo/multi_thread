699ee46ff2b84338768d79d5848f44405b4028f8
姚想 <yx4@meitu.com>
2019-09-21 20:05:47 +0800

    改成string 能够运行但有隐患

--- lesson-4/main.cpp
+++ lesson-4/main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <thread>
 using namespace std;
-void myprint(const int &i, char * p){
+void myprint(const int &i, const string &p){//改晨string 隐式转换
     cout<<i<<endl; //**不是引用，是值传递**
-    cout<<p<<endl;// 指针在detach绝对有问题，不建议传指针
+    cout<<p.c_str()<<endl;// 指针在detach绝对有问题，不建议传指针
 }
 int main(int argc, const char * argv[]) {
     // 传递临时变量作为线程参数
