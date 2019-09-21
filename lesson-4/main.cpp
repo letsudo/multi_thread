c278aa18b517f1679b5bf6feecbac668ac53eac1
姚想 <yx4@meitu.com>
2019-09-21 20:54:02 +0800

    [feature] 临时类对象 线程id

--- lesson-4/main.cpp
+++ lesson-4/main.cpp
@@ -1,18 +1,32 @@
 #include <iostream>
 #include <thread>
 using namespace std;
-void myprint(const int &i, const string &p){//改晨string 隐式转换
+class TA
+{
+public:
+    int m_i;
+    TA(int i):m_i(i){
+        std::cout<<"构造函数"<<"thread_id: "<<std::this_thread::get_id()<<std::endl;
+    }
+    TA(const TA &ta):m_i(ta.m_i){
+        std::cout<<"拷贝构造函数"<<"thread_id: "<<std::this_thread::get_id()<<std::endl;
+    }
+    ~TA(){
+        std::cout<<"析构函数"<<"thread_id: "<<std::this_thread::get_id()<<std::endl;
+    }
+};
+
+void myprint(const int i, const TA &p){//改晨string 隐式转换
     cout<<i<<endl; //**不是引用，是值传递**
-    cout<<p.c_str()<<endl;// 指针在detach绝对有问题，不建议传指针
+    std::cout<<"myprint"<<"thread_id: "<<std::this_thread::get_id()<<std::endl;
 }
 int main(int argc, const char * argv[]) {
     // 传递临时变量作为线程参数
     int var =1;
-    int &vary = var;
-    char buf[] = "this is a text";
-    thread t(myprint, var, buf);
-//    t.join();
-    t.detach();
+    std::cout<<"主线程id: "<<"thread_id: "<<std::this_thread::get_id()<<std::endl;
+    thread t(myprint, 1, TA(var));// 采用临时构造传递参数是可行的，这个转换到stirng的过程先于main函数的结束，会先完成转换，避免临时变量失效，视频中采用字写类测试，此处会调用构造函数和拷贝构造函数
+    t.join();
+//    t.detach();
     cout<<"Hi in main"<<endl;
     return 0;
 }
