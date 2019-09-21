d2e5e5781495aace4820862b2112aa180f4ca67f
姚想 <yx4@meitu.com>
2019-09-21 17:26:06 +0800

    [feature] 说明 thread调用函数

--- lesson-1/main.cpp
+++ lesson-1/main.cpp
@@ -7,7 +7,9 @@ void function(){
 
 
 int main(int argc, const char * argv[]) {
-    std::thread t1(function);//创建t1子线程，并执行
+    std::thread t1(function);//创建t1子线程，并执行，functionx是线程执行的入口
+    
+    //join加入汇合、阻塞
 //    t1.join();//主线程等待t1子线程执行完毕
     t1.detach();//不等待， 一旦被detach 就不能被join了
     if(t1.joinable()){
