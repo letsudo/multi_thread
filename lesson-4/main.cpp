9e891435d1e7e798da53df471d0531f44e196a1c
姚想 <yx4@meitu.com>
2019-09-22 09:45:55 +0800

    传类对象，虽然方法中是引用，但还是会调用拷贝构造函数

--- lesson-4/main.cpp
+++ lesson-4/main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 class TA
 {
 public:
-    int m_i;
+    mutable int m_i;
     TA(int i):m_i(i){
         std::cout<<"构造函数"<<"thread_id: "<<std::this_thread::get_id()<<std::endl;
     }
@@ -17,14 +17,14 @@ public:
 };
 
 void myprint(const int i, const TA &p){//改晨string 隐式转换
-    cout<<i<<endl; //**不是引用，是值传递**
+    p.m_i = 199;
     std::cout<<"myprint"<<"thread_id: "<<std::this_thread::get_id()<<std::endl;
 }
 int main(int argc, const char * argv[]) {
     // 传递临时变量作为线程参数
-    int var =1;
     std::cout<<"主线程id: "<<"thread_id: "<<std::this_thread::get_id()<<std::endl;
-    thread t(myprint, 1, TA(var));// 采用临时构造传递参数是可行的，这个转换到stirng的过程先于main函数的结束，会先完成转换，避免临时变量失效，视频中采用字写类测试，此处会调用构造函数和拷贝构造函数
+    TA ta(10);
+    thread t(myprint, 1, ta);// 采用临时构造传递参数是可行的，这个转换到stirng的过程先于main函数的结束，会先完成转换，避免临时变量失效，视频中采用字写类测试，此处会调用构造函数和拷贝构造函数
     t.join();
 //    t.detach();
     cout<<"Hi in main"<<endl;
