4d7f24bd0e7f796cb3d37cfd340928e8811ddce9
姚想 <yx4@meitu.com>
2019-09-22 23:01:34 +0800

    互斥量mutex lock & unlock 成对出现

--- lesson-5/main.cpp
+++ lesson-5/main.cpp
@@ -2,30 +2,43 @@
 #include <thread>
 #include <vector>
 #include <list>
+#include <mutex>
 using namespace std;
 class A{
 public:
     void infunc(){
-        for(int i=0; i<1000; i++){
+        for(int i=0; i<5; i++){
+            my_mutex.lock();
             list.push_back(i);
+            my_mutex.unlock();
             cout<<"infunc push back"<<i<<endl;
         }
     }
     
+    bool out_proc(){
+        my_mutex.lock();
+        if(!list.empty()){
+            cout<<"outfunc front"<<list.front()<<endl;
+            list.pop_front();
+            my_mutex.unlock();
+            return true;
+        }
+        my_mutex.unlock();
+        return false;
+    }
+    
+    
     void outfunc(){
-        for(int i=0; i<1000; i++){
-            if(!list.empty()){
-                cout<<"outfunc front"<<list.front()<<endl;
-                list.pop_front();
-            }else{
-                cout<<"list empty"<<endl;
-            }
+        for(int i=0; i<5; i++){
+            bool result = out_proc();
+            if(result){cout<<"rm front success!";}
         }
         cout<<"end"<<endl;
     }
 
 private:
     list<int> list;
+    mutex my_mutex;
 };
 
 int main(int argc, const char * argv[]) {
