22ed73cf49d7a77d096e9ad8179a9b5248c9ec67
姚想 <yx4@meitu.com>
2019-09-22 09:56:51 +0800

    智能指针作为参数传递

--- lesson-4/main.cpp
+++ lesson-4/main.cpp
@@ -16,18 +16,16 @@ public:
     }
 };
 
-void myprint(const int i, const TA &p){//改晨string 隐式转换
-    p.m_i = 199;
+void myprint(const int i, unique_ptr<int> p){//改晨string 隐式转换
     std::cout<<"myprint"<<"thread_id: "<<std::this_thread::get_id()<<std::endl;
 }
 int main(int argc, const char * argv[]) {
     // 传递临时变量作为线程参数
     std::cout<<"主线程id: "<<"thread_id: "<<std::this_thread::get_id()<<std::endl;
-    TA ta(10);
-    thread t(myprint, 1, ref(ta));// 采用临时构造传递参数是可行的，这个转换到stirng的过程先于main函数的结束，会先完成转换，避免临时变量失效，视频中采用字写类测试，此处会调用构造函数和拷贝构造函数
+    unique_ptr<int> ptr(new int(100));
+    thread t(myprint, 1, std::move(ptr));// 采用临时构造传递参数是可行的，这个转换到stirng的过程先于main函数的结束，会先完成转换，避免临时变量失效，视频中采用字写类测试，此处会调用构造函数和拷贝构造函数
     t.join();
 //    t.detach();
-    cout<<"Hi in main"<<ta.m_i<<endl;
     cout<<"Hi in main"<<endl;
     return 0;
 }
