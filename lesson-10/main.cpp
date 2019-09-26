1c06aeaf7415cf1010e80be6a54a636d558be756
姚想 <yx4@meitu.com>
2019-09-26 08:43:49 +0800

    [feature] std::launch::async

--- lesson-10/main.cpp
+++ lesson-10/main.cpp
@@ -19,30 +19,32 @@ public:
 
 int main(int argc, const char * argv[]) {
     
-    // 一 async，future 创建后台任务并返回值
-    //希望子线程返回一个结果
-    //std::async 是函数模板，用来启动一个异步任务， 启动后返回一个std::future 对象
-    //std::future  类模版
-    //启动一个异步任务： 就是自动创建一个线程并开始执行线程的入口函数，返回future包含线程的返回结果，可以通过调用future的get（）获得结果
-    //futrue 在线程执行完毕后能拿到的结果
-    
-    //通过向线程std::async()传递一个参数，该参数是std::launch 枚举类型
-    //a. std::launch:deferred 表示线程的入口函数调用被延迟std::future的wait 和get 调用的时候在执行。如果wait 和get没有调用线程都没有被创建
-    //    main thread id:    0x1000b15c0
-    //    continue!
-    //    12
-    //    my thread start thread id:    0x1000b15c0
-    //    my thread end thread id:    0x1000b15c0
-    //    5
-    //    done!
-    //    Program ended with exit code: 0
-    // 延迟调用，没有创建线程，是在追线程中调用线程入口函数
+//     一 async，future 创建后台任务并返回值
+//    希望子线程返回一个结果
+//    std::async 是函数模板，用来启动一个异步任务， 启动后返回一个std::future 对象
+//    std::future  类模版
+//    启动一个异步任务： 就是自动创建一个线程并开始执行线程的入口函数，返回future包含线程的返回结果，可以通过调用future的get（）获得结果
+//    futrue 在线程执行完毕后能拿到的结果
+//    
+//    通过向线程std::async()传递一个参数，该参数是std::launch 枚举类型
+//    a. std::launch:deferred 表示线程的入口函数调用被延迟std::future的wait 和get 调用的时候在执行。如果wait 和get没有调用线程都没有被创建
+//        main thread id:    0x1000b15c0
+//        continue!
+//        12
+//        my thread start thread id:    0x1000b15c0
+//        my thread end thread id:    0x1000b15c0
+//        5
+//        done!
+//        Program ended with exit code: 0
+//     延迟调用，没有创建线程，是在追线程中调用线程入口函数
+//    
+//    b. std::launch::async 在调用async函数的时候就启动线程, 不需要get就直接执行，这个是async的默认参数
     
     A a;
     int temp =12;
     cout<<"main thread id:\t"<<this_thread::get_id()<<endl;
 //    std::future<int> result = std::async(mythread); //创建一个线程并开始执行
-    std::future<int> result = std::async(std::launch::deferred,&A::mythread, &a, temp); //创建一个线程并开始执行
+    std::future<int> result = std::async(std::launch::async,&A::mythread, &a, temp); //创建一个线程并开始执行
     cout<<"continue!"<<endl;
     cout<<result.get()<<endl;//卡在这里等待mythread执行完拿到结果
 //    cout<<result.get()<<endl;//只能调用一次，调用多次崩溃
