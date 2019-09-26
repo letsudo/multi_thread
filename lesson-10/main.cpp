5685dab140c4fbec6ecd11bb6f3215dcf657efce
姚想 <yx4@meitu.com>
2019-09-26 08:51:45 +0800

    [feature] std::packaged_task 包装入口函数

--- lesson-10/main.cpp
+++ lesson-10/main.cpp
@@ -5,17 +5,26 @@
 #include <mutex>
 #include <future>
 using namespace std;
-class A{
-public:
-    int mythread(int para){
-        cout<<para<<endl;
-        cout<<"my thread start thread id:\t"<<this_thread::get_id()<<endl;
-        chrono::milliseconds dura(5000);
-        this_thread::sleep_for(dura);
-        cout<<"my thread end thread id:\t"<<this_thread::get_id()<<endl;
-        return 5;
-    }
-};
+//class A{
+//public:
+//    int mythread(int para){
+//        cout<<para<<endl;
+//        cout<<"my thread start thread id:\t"<<this_thread::get_id()<<endl;
+//        chrono::milliseconds dura(5000);
+//        this_thread::sleep_for(dura);
+//        cout<<"my thread end thread id:\t"<<this_thread::get_id()<<endl;
+//        return 5;
+//    }
+//};
+
+int mythread(int para){
+    cout<<para<<endl;
+    cout<<"my thread start thread id:\t"<<this_thread::get_id()<<endl;
+    chrono::milliseconds dura(5000);
+    this_thread::sleep_for(dura);
+    cout<<"my thread end thread id:\t"<<this_thread::get_id()<<endl;
+    return 5;
+}
 
 int main(int argc, const char * argv[]) {
     
@@ -25,7 +34,7 @@ int main(int argc, const char * argv[]) {
 //    std::future  类模版
 //    启动一个异步任务： 就是自动创建一个线程并开始执行线程的入口函数，返回future包含线程的返回结果，可以通过调用future的get（）获得结果
 //    futrue 在线程执行完毕后能拿到的结果
-//    
+//
 //    通过向线程std::async()传递一个参数，该参数是std::launch 枚举类型
 //    a. std::launch:deferred 表示线程的入口函数调用被延迟std::future的wait 和get 调用的时候在执行。如果wait 和get没有调用线程都没有被创建
 //        main thread id:    0x1000b15c0
@@ -37,18 +46,29 @@ int main(int argc, const char * argv[]) {
 //        done!
 //        Program ended with exit code: 0
 //     延迟调用，没有创建线程，是在追线程中调用线程入口函数
-//    
+//
 //    b. std::launch::async 在调用async函数的时候就启动线程, 不需要get就直接执行，这个是async的默认参数
     
-    A a;
-    int temp =12;
+//    A a;
+//    int temp =12;
+//    cout<<"main thread id:\t"<<this_thread::get_id()<<endl;
+////    std::future<int> result = std::async(mythread); //创建一个线程并开始执行
+//    std::future<int> result = std::async(std::launch::async,&A::mythread, &a, temp); //创建一个线程并开始执行
+//    cout<<"continue!"<<endl;
+//    cout<<result.get()<<endl;//卡在这里等待mythread执行完拿到结果
+////    cout<<result.get()<<endl;//只能调用一次，调用多次崩溃
+////    result.wait() //仅等待线程返回，并不获取结果
+//    cout<<"done!"<<endl;
+    
+    
+    // 二、 std::packaged_task 打包任务，把任务包装起来
+    // 是个类模板，他的参数是各种可调用对象，通过std::packaged_task把d各种调用对象包装起来，方便作为线程入口函数
     cout<<"main thread id:\t"<<this_thread::get_id()<<endl;
-//    std::future<int> result = std::async(mythread); //创建一个线程并开始执行
-    std::future<int> result = std::async(std::launch::async,&A::mythread, &a, temp); //创建一个线程并开始执行
-    cout<<"continue!"<<endl;
+    std::packaged_task<int(int)> mypt(mythread);//我们把函数mythread通过packaged_task包装起来
+    std::thread t1(std::ref(mypt),1);//线程直接开始执行，第二个参数作为线程入口函数的参数
+    t1.join();//等待线程执行完毕
+    std::future<int> result = mypt.get_future();//std::future 包含线程入口函数的返回结果，
     cout<<result.get()<<endl;//卡在这里等待mythread执行完拿到结果
-//    cout<<result.get()<<endl;//只能调用一次，调用多次崩溃
-//    result.wait() //仅等待线程返回，并不获取结果
     cout<<"done!"<<endl;
     return 0;
 }
