5ae9388acbc7c7e0fcdb59b7ff379dc48fae45c0
姚想 <yx4@meitu.com>
2019-09-27 09:08:04 +0800

    [feature] shared_future 可以多次get 复制。普通的是移动

--- lesson-11/main.cpp
+++ lesson-11/main.cpp
@@ -5,7 +5,6 @@
 #include <mutex>
 #include <future>
 using namespace std;
-
 int mythread(int para){
     cout<<para<<endl;
     cout<<"my thread start thread id:\t"<<this_thread::get_id()<<endl;
@@ -15,25 +14,34 @@ int mythread(int para){
     return 5;
 }
 
-int main(int argc, const char * argv[]) {
+int mythread2(std::shared_future<int> &temp){
+    cout<<"my thread start thread id:\t"<<this_thread::get_id()<<endl;
+    chrono::milliseconds dura(5000);
+    this_thread::sleep_for(dura);
+    cout<<"my thread end thread id:\t"<<this_thread::get_id()<<endl;
+    auto result = temp.get();
+    cout<<result<<endl;
+    return 0;
+}
 
-    int temp =12;
-    cout<<"main thread id:\t"<<this_thread::get_id()<<endl;
-//    std::future<int> result = std::async(mythread); //创建一个线程并开始执行
-    std::future<int> result = std::async(mythread,temp); //创建一个线程并开始执行
-    std::future_status status = result.wait_for(std::chrono::seconds(6)); //等待一秒， 如果是deferred这里不会等待
-     cout<<"continue!"<<endl;
-    if( status == std::future_status::timeout){ //超时
-        cout<<"time out"<<endl;
-    }else if( status == std::future_status::ready){
-        cout<<"ready"<<endl;
-         cout<<result.get()<<endl;//卡在这里等待mythread执行完拿到结果
-    }else if (status == std::future_status::deferred){
-        // 如果async 的第一个参数被设置为deferred， 则次条件成立
-         cout<<"deferred"<<endl;
-         cout<<result.get()<<endl;//卡在这里等待mythread执行完拿到结果
-    }
+int main(int argc, const char * argv[]) {
+    //三、std::shared_future 类模板，普通future的get函数是将数据转移，类似于move， shared_future 是的get是复制
+    
    
+    cout<<"main thread id:\t"<<this_thread::get_id()<<endl;
+    std::packaged_task<int(int)> mypt(mythread);//我们把函数mythread通过packaged_task包装起来
+    std::thread t1(std::ref(mypt),1);//线程直接开始执行，第二个参数作为线程入口函数的参数
+    t1.join();//等待线程执行完毕
+    std::future<int> result = mypt.get_future();//std::future 包含线程入口函数的返回结果，
+    bool val = result.valid();
+//    std::shared_future<int> result_s(std::move(result));
+    std::shared_future<int> result_s(result.share()); //执行完毕后result_s 里面有值， 而result里面空了
+    val = result.valid();
+    
+    auto result___ = result_s.get();
+    result___ = result_s.get();
+    std::thread t2(mythread2, std::ref(result_s));
+    t2.join();
     cout<<"done!"<<endl;
     return 0;
 }
