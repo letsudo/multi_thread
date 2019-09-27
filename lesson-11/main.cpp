8b7abcc79933994415766ae3e49cba368e482745
姚想 <yx4@meitu.com>
2019-09-27 09:26:20 +0800

    多线程对同一变量操作可能存在问题

--- lesson-11/main.cpp
+++ lesson-11/main.cpp
@@ -5,43 +5,25 @@
 #include <mutex>
 #include <future>
 using namespace std;
-int mythread(int para){
-    cout<<para<<endl;
-    cout<<"my thread start thread id:\t"<<this_thread::get_id()<<endl;
-    chrono::milliseconds dura(5000);
-    this_thread::sleep_for(dura);
-    cout<<"my thread end thread id:\t"<<this_thread::get_id()<<endl;
-    return 5;
-}
 
-int mythread2(std::shared_future<int> &temp){
-    cout<<"my thread start thread id:\t"<<this_thread::get_id()<<endl;
-    chrono::milliseconds dura(5000);
-    this_thread::sleep_for(dura);
-    cout<<"my thread end thread id:\t"<<this_thread::get_id()<<endl;
-    auto result = temp.get();
-    cout<<result<<endl;
-    return 0;
+int g_int =0;
+mutex lock;
+
+void mythread(){
+    for(int i=0; i<1000000 ;i++){
+        g_int++;
+    }
 }
 
 int main(int argc, const char * argv[]) {
-    //三、std::shared_future 类模板，普通future的get函数是将数据转移，类似于move， shared_future 是的get是复制
-    
-   
-    cout<<"main thread id:\t"<<this_thread::get_id()<<endl;
-    std::packaged_task<int(int)> mypt(mythread);//我们把函数mythread通过packaged_task包装起来
-    std::thread t1(std::ref(mypt),1);//线程直接开始执行，第二个参数作为线程入口函数的参数
-    t1.join();//等待线程执行完毕
-    std::shared_future<int> result_s = mypt.get_future();//std::future 包含线程入口函数的返回结果，
-//    bool val = result.valid();
-////    std::shared_future<int> result_s(std::move(result));
-//    std::shared_future<int> result_s(result.share()); //执行完毕后result_s 里面有值， 而result里面空了
-//    val = result.valid();
-//
-//    auto result___ = result_s.get();
-//    result___ = result_s.get();
-    std::thread t2(mythread2, std::ref(result_s));
+    // 三、 原子操作 std::atomic
+    //互斥量： 多线程中保护共享数据
+    //两个线程，对一个变量进行操作，一个线程读值， 一个线程写值
+    //原子操作：不会被打断的执行片段，原子操作的效率比加锁要高，但锁可以对代码段进行加锁，原子操作一一般仅针对一个变量
+    thread t1(mythread);
+    thread t2(mythread);
+    t1.join();
     t2.join();
-    cout<<"done!"<<endl;
+    cout<<g_int<<endl;
     return 0;
 }
